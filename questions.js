1bff5cc44f637d11983f06b2544d8d31ab490583
ClintTheEngineer <81938100+ClintTheEngineer@users.noreply.github.com>
2023-02-04 17:42:49 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,20 +1,20 @@
 const request = new XMLHttpRequest()
+let questions;
 
 function onload(){
   const data = JSON.parse(this.response)
   localStorage.setItem('Question List', JSON.stringify(data))
+  questions = JSON.parse(localStorage,getIten('Question List'))
+  shuffleQuestions(questions)
 }
 
 request.open('GET', `https://quiz-api.cyclic.app/api/questions`, true)
 request.addEventListener('load', onload)
 request.send()
 
-let questions = JSON.parse(localStorage.getItem('Question List'))
-
-
 function shuffleQuestions(questions){
   questions.sort(() => Math.random() - 0.5)
+return questions
 }
-shuffleQuestions(questions)
 
 
